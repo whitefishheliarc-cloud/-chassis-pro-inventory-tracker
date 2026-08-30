7ebd8bc9c112e8180a9ff21cedd6a9ac1b1ea8c5
whitefishheliarc-cloud <whitefishheliarc@yahoo.com>
2026-08-30 18:29:54 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='chassis-pro-v4';
+const CACHE='chassis-pro-v5';
 const SHELL=['./','./index.html','./manifest.webmanifest','./assets/mockup.png','./assets/icon-192.png','./assets/icon-512.png','./assets/chassis-pro-logo.jpg','./assets/car-81.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
